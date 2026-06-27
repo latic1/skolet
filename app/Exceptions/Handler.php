b2b2a99da37b6c223901e9c657d19a1d538afb33
latc1 <latclatif2@gmail.com>
2026-06-27 11:07:15 +0000

    fix: wire Sentry captureException into the exception handler reportable callback

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -30,7 +30,9 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
         });
 
         // Return JSON error responses for all /api/* routes
